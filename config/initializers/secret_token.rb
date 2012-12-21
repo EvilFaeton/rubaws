d9465353849232912d2b44c45f31888984f09156
Sergey Efremov <efremov.sergey@gmail.com>
2012-12-21 14:32:12 +0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubaws::Application.config.secret_token = '648b955853fd53bb73da4f452b4a2cfe271daaee21afb56ca30d96eec577c1326573640723d0b565a856337649ac3ab6a2323fdfc2ba50380d6ac9492988a5bd'
